bb4e332156e4eda2db09531806cce4afb4490d00
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-10-31 13:55:13 -0400

    data add data mock

--- schema.js
+++ schema.js
@@ -1,17 +1,95 @@
-const {  makeExecutableSchema } = require('graphql-tools');
+const {  makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools');
+const casual = require('casual');
+
 
 const typeDefs = `
+# Esto es un curso
+type Curso {
+  id: ID!
+  titulo: String!
+  # tomala
+  descripcion: String!
+  profesor: Profesor
+  rating: Float @deprecated(reason: "no va más")
+  comentarios: [Comentario]
+}
 
-  type Course {
-    id: ID!
-    title: String!
-  }
+type Profesor {
+  id: ID!
+  nombre: String!
+  nacionalidad: String!
+  genero: Genero
+  cursos: [Curso]
+}
 
-  type Query {
-    courses: [Course!]!
-  }
+enum Genero {
+  MASCULINO
+  FEMENINO
+}
+
+type Comentario {
+  id: ID!
+  nombre: String!
+  cuerpo: String!
+}
+
+type Query {
+  cursos: [Curso]
+  profesores: [Profesor]
+  curso(id: Int): Curso
+  profesor(id: Int): Profesor
+}
 `;
 
-const executableSchema = makeExecutableSchema({ typeDefs });
+const resolvers = {
+  Query: {
+    cursos: () => {
+      return [
+        {
+          id : '121212',
+          titulo : 'asass',
+        },
+        {
+          id : '12sd1212',
+          titulo : 'asass',
+        }
+      ]
+    }
+  },
+  Curso: {
+    id: () => {
+      return 'sd';
+    },
+    profesor: () => {
+      return {
+        id: 12
+      }
+    }
+  }
+}
 
-module.exports = executableSchema;
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+addMockFunctionsToSchema({
+  schema,
+  mocks: {
+    Curso: () => {
+      return {
+        id: casual.uuid,
+        titulo : casual.title,
+      }
+    },
+    Profesor: () => {
+      return {
+        id: casual.uuid,
+        nombre : casual.name,
+      }
+    }
+  },
+  preserveResolvers: false
+})
+
+module.exports = schema;
